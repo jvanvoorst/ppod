af7e1220866917d431ecedd0b6a1dec050b43be7
justin.vanvoorst@gmail.com <justin.vanvoorst@gmail.com>
2016-11-16 17:34:36 -0700

    add database connection

--- src/resources/config.php
+++ src/resources/config.php
@@ -2,10 +2,10 @@
 
 $config = array(
 	"db" => array(
-		"dbname" => "",
-		"username" => "",
-		"password" => "",
-		"host" => ""
+		"dbName" => "project",
+		"userName" => "project",
+		"password" => "project",
+		"host" => "mysql"
 	),
 	"pqApi" => array(
 		"key" => "7be2d31f-05c7-4937-afb8-4ff0ed98cd17",
