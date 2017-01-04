ee813307746d0b84afd79eb2aa958a2ad8be19e1
justin.vanvoorst@gmail.com <justin.vanvoorst@gmail.com>
2017-01-04 13:39:45 -0700

    reworked email config

--- src/php/email.php
+++ src/php/email.php
@@ -3,14 +3,10 @@
 include('./../resources/config.php');
 require_once 'Mail.php';
 
-$recipients = 'justin.vanvoorst@colorado.edu';
-
-$body = 'adding a little more so maybe it works';
-
 // Create the mail object using the Mail::factory method
-$mail_object =& Mail::factory("smtp", $config["smtp"]); 
+$mail_object =& Mail::factory("smtp", $mail["smtp"]); 
 
-$mail_object->send($config["recipients"], $config["headerOrder"], $body);
+$mail_object->send($mail["recipients"], $mail["headerOrder"], $mail["bodyOrder"]);
 
 if (PEAR::isError($mail)) {
       echo("<p>" . $mail->getMessage() . "</p>");
